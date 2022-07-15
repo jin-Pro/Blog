00b47ad2b7c3a2db3ac9156e08641bde2d42e9ad
jin-Pro <dnjun2@ajou.ac.kr>
2022-07-16 03:23:12 +0900

    feat: wrapPromise for Suspense

---
+++ src/_Common/Util/Suspense.tsx
@@ -0,0 +1,26 @@
+type wrapPromiseType<T> = (promise: Promise<T>) => any;
+export const wrapPromise: wrapPromiseType<any> = (promise) => {
+  let status = "pending";
+  let result: any;
+  let suspender = promise.then(
+    (r) => {
+      status = "success";
+      result = r;
+    },
+    (e) => {
+      status = "error";
+      result = e;
+    }
+  );
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      } else if (status === "error") {
+        throw result;
+      } else if (status === "success") {
+        return result;
+      }
+    },
+  };
+};
